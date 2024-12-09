ca7b0d9ba54aac94e87f046f3d99670ef75b6401
aayushxadhikari <aayushxadhikari.com>
2024-12-09 22:17:03 +0545

    Adding constants

---
+++ lib/utils/helpers/helper_functions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+class AHelperFUnctions {
+  static Color? getColor(String value) {
+    if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Red') {
+      return Colors.red;
+    } else if
+  }
+}
